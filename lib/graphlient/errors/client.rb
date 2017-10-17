8f89d118d93db12100850261598f4e6b93961875
dblock <dblock@dblock.org>
2017-10-17 19:27:13 -0400

    Fix: typo.

--- lib/graphlient/errors/client.rb
+++ lib/graphlient/errors/client.rb
@@ -2,6 +2,7 @@ module Graphlient
   module Errors
     class Client < StandardError
       attr_reader :inner_exception
+
       def initialize(message, inner_exception = nil)
         super(message)
         @inner_exception = inner_exception
--- spec/graphlient/client_spec.rb
+++ spec/graphlient/client_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 
 describe Graphlient::Client do
-  let(:graphql_endpoint) { 'http://graph.biz/gprahql' }
+  let(:graphql_endpoint) { 'http://graph.biz/graphql' }
+
   let(:request_headers) do
     {
       'Authorization' => 'Bearer 1231',
       'Content-Type' => 'application/json'
     }
   end
+
   let(:graphql_client) { Graphlient::Client.new(graphql_endpoint, headers: request_headers) }
+
   describe '#query' do
     let(:response) do
       graphql_client.query do
@@ -20,9 +23,10 @@ describe Graphlient::Client do
         end
       end
     end
+
     describe 'success' do
       let!(:graphql_post_schema_request) do
-        stub_request(:post, 'http://graph.biz/gprahql').to_return(
+        stub_request(:post, 'http://graph.biz/graphql').to_return(
           { body: DummySchema.to_json },
           { body:   {
             data: {
@@ -36,6 +40,7 @@ describe Graphlient::Client do
           }.to_json }
         )
       end
+
       it 'returns error for invalid schema' do
         expect do
           graphql_client.query do
@@ -50,14 +55,17 @@ describe Graphlient::Client do
           expect(e.to_s).to eq "Field 'invoice' doesn't exist on type 'RootQuery'"
         end
       end
+
       it 'returns expected query with block' do
         invoices = response.data.invoices
         expect(invoices.first.id).to eq '1231'
         expect(invoices.first.fee_in_cents).to eq 20_000
       end
     end
+
     describe 'failure' do
-      let!(:graphql_post_request) { stub_request(:post, 'http://graph.biz/gprahql').to_return(status: [500, 'Internal Server Error']) }
+      let!(:graphql_post_request) { stub_request(:post, 'http://graph.biz/graphql').to_return(status: [500, 'Internal Server Error']) }
+
       it 'fails with an exception' do
         expect do
           response
