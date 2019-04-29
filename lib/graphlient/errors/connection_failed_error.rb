480938489214ecaf2b9bf8bf5bd859154a475793
Brian Coates <brian.coates@life.church>
2019-04-29 13:40:26 -0500

    Removes unnesecary inner exception

--- lib/graphlient/errors/connection_failed_error.rb
+++ lib/graphlient/errors/connection_failed_error.rb
@@ -1,10 +1,6 @@
 module Graphlient
   module Errors
     class ConnectionFailedError < ServerError
-      def initialize(inner_exception)
-        @inner_exception = inner_exception
-        @response = inner_exception.wrapped_exception
-      end
     end
   end
 end
